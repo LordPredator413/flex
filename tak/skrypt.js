9a8fa3e88a651e0c3a23656e090258f3b82352da
LordPredator413 <50619009+LordPredator413@users.noreply.github.com>
2020-09-07 13:04:04 +0200

    Add files via upload

---
+++ tak/skrypt.js
@@ -0,0 +1,8 @@
+<script>
+            function Kolor(){
+                var r=getElementbyid("r1").value;
+                var g=getElementbyid("g1").value;
+                var b=getElementbyid("b1").value;
+                document.
+            }
+</script>
