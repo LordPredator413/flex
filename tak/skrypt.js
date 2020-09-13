d07bf69675907fee764c2ec23fed9b28ed8b3478
LordPredator413 <50619009+LordPredator413@users.noreply.github.com>
2020-09-13 18:35:46 +0200

    Delete skrypt.js

--- tak/skrypt.js
+++
@@ -1,8 +0,0 @@
-<script>
-            function Kolor(){
-                var r=getElementbyid("r1").value;
-                var g=getElementbyid("g1").value;
-                var b=getElementbyid("b1").value;
-                document.
-            }
-</script>
