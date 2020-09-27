51566e4910f24afe52fc534fbda7232ec5697c9f
LordPredator413 <50619009+LordPredator413@users.noreply.github.com>
2020-09-27 20:31:45 +0200

    Add files via upload

--- flex 3.0/apk.js
+++ flex 3.0/apk.js
@@ -1,17 +1,20 @@
+// stała navSlide jest równa anonimowej funkcji
 const navSlide = () => {
+  // Stałe 
   const burger = document.querySelector('.burger');
   const nav = document.querySelector('.nav-link');
   const navLinks = document.querySelectorAll('.nav-link li');
 
+  //Działanie burgera
   burger.addEventListener('click', () => {
+    // Rozsuniecie listy 
     nav.classList.toggle('nav-active');
 
   });
 
-
+    // Animacja chowania
   navLinks.forEach((link, index) => {
-    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5}s`;
-    console.log(index / 5);
+    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 1}s`;
   })
 }
 
